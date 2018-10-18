e0b19bb5eda0000bc03f62f56bfc68287120ecc5
verno <vernon.lyons@gmail.com>
2018-10-18 14:22:34 -0500

    Initial character edit page.

--- app/controllers/CharacterController.java
+++ app/controllers/CharacterController.java
@@ -1,7 +1,6 @@
 package controllers;
 
-import models.CharacterDetails;
-import models.GameCharacter;
+import models.*;
 import play.data.FormFactory;
 import play.db.jpa.JPAApi;
 import play.db.jpa.Transactional;
@@ -44,5 +43,41 @@ public class CharacterController extends Controller
         List<CharacterDetails> characterDetails = jpaApi.em().createQuery(sql, CharacterDetails.class).getResultList();
 
         return ok(views.html.characterdetails.render(characterDetails));
+    }
+
+    @Transactional
+    public Result getEditCharacter(int characterId)
+    {
+        String characterSQL = "SELECT c from GameCharacter c WHERE characterId = :characterId";
+        GameCharacter gameCharacter = jpaApi.em().createQuery(characterSQL, GameCharacter.class).setParameter("characterId", characterId).getSingleResult();
+
+        String nextCharacterIdSQL = "SELECT NEW models.CharacterId(MIN(characterId)) FROM GameCharacter c WHERE characterId > :characterId";
+        CharacterId nextCharacterId = jpaApi.em().createQuery(nextCharacterIdSQL, CharacterId.class).setParameter("characterId", characterId).getSingleResult();
+
+        if (nextCharacterId.getId() == null)
+        {
+            String minCharacterIdSQL = "SELECT NEW models.CharacterId(MIN(characterId)) FROM GameCharacter c";
+            nextCharacterId = jpaApi.em().createQuery(minCharacterIdSQL, CharacterId.class).getSingleResult();
+        }
+
+        String previousCharacterIdSQL = "SELECT NEW models.CharacterId(MAX(characterId)) FROM GameCharacter c WHERE characterId < :characterId";
+        CharacterId previousCharacterId = jpaApi.em().createQuery(previousCharacterIdSQL, CharacterId.class).setParameter("characterId", characterId).getSingleResult();
+
+        if (previousCharacterId.getId() == null)
+        {
+            String maxCharacterIdSQL = "SELECT NEW models.CharacterId(MAX(characterId)) FROM GameCharacter c";
+            previousCharacterId = jpaApi.em().createQuery(maxCharacterIdSQL, CharacterId.class).getSingleResult();
+        }
+
+        String raceSQL = "SELECT r FROM Race r ORDER BY raceName";
+        List<Race> races = jpaApi.em().createQuery(raceSQL, Race.class).getResultList();
+
+        String classSQL = "SELECT ct FROM ClassType ct ORDER BY classTypeName";
+        List<ClassType> classTypes = jpaApi.em().createQuery(classSQL, ClassType.class).getResultList();
+
+        String backgroundSQL = "SELECT b FROM Background b ORDER BY backgroundName";
+        List<Background> backgrounds = jpaApi.em().createQuery(backgroundSQL, Background.class).getResultList();
+
+        return ok(views.html.editcharacter.render(gameCharacter, races, classTypes, backgrounds, previousCharacterId.getId(), nextCharacterId.getId()));
     }
 }
